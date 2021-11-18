68d02bd1413ff370c40ee9d53d4cf52d98ec0bc0
ilhambagirov <ilhambb@code.edu.az>
2021-11-18 04:31:26 +0400

    Navbar(Ongoing 2)

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import Header from './components/Layout/Header';
+import Header from './components/Layout/Header/Header';
+import Main from './components/Layout/Main/Main';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Main/>
     </Fragment>
   );
 }
---
+++ client-app/src/components/Layout/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Main.scss'
+
+export default function Main() {
+    return (
+        <main>
+            <div className='test'>
+
+            </div>
+        </main>
+    )
+}
