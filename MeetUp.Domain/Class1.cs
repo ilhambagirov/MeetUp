7b1651b3847e42893a80c4b0a87f654e9f73b437
ilhambagirov <ilhambb@code.edu.az>
2021-12-01 18:02:13 +0400

    Software Architecture (Initial)

---
+++ MeetUp.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MeetUp.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ MeetUp.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MeetUp.Persistence
+{
+    public class Class1
+    {
+    }
+}
